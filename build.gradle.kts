9ef61b9bf11de1f6255808198849405cd7b2ce59
Patrick <patway99@gmail.com>
2021-07-08 17:30:58 -0700

    Initial micronaut2-to-3 recipe  - upgrades micronaut parent version to 3.x  - changes package io.micronaut.configuration.cassandra to io.micronaut.cassandra  - changes javax.inject to jakarta.inject

--- build.gradle.kts
+++ build.gradle.kts
@@ -89,6 +89,7 @@ dependencies {
     implementation("org.openrewrite:rewrite-java:${rewriteVersion}")
     implementation("org.openrewrite:rewrite-maven:${rewriteVersion}")
     runtimeOnly("org.openrewrite:rewrite-java-11:${rewriteVersion}")
+    runtimeOnly("org.openrewrite.recipe:rewrite-migrate-java:${rewriteVersion}")
 
     // eliminates "unknown enum constant DeprecationLevel.WARNING" warnings from the build log
     // see https://github.com/gradle/kotlin-dsl-samples/issues/1301 for why (okhttp is leaking parts of kotlin stdlib)
