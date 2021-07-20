1e4235a04680ff3382c2d16baad0038e322d5086
Patrick <patway99@gmail.com>
2021-07-20 14:13:01 -0700

    Adding  OncePerRequestServerFilter to HttpServerFilter recipe. Fixes #7

---
+++ src/main/java/org/openrewrite/java/micronaut/OncePerRequestHttpServerFilterToHttpServerFilter.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.micronaut;
+
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.internal.ListUtils;
+import org.openrewrite.internal.lang.Nullable;
+import org.openrewrite.java.ChangeMethodName;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.search.UsesType;
+import org.openrewrite.java.tree.*;
+import org.openrewrite.marker.Markers;
+
+import java.util.UUID;
+
+public class OncePerRequestHttpServerFilterToHttpServerFilter extends Recipe {
+    private static final String ONCE_PER_REQUEST_FILTER_FQN = "io.micronaut.http.filter.OncePerRequestHttpServerFilter";
+
+    @Override
+    public String getDisplayName() {
+        return "Convert OncePerRequestServerFilter extensions to HttpServerFilter implementations";
+    }
+
+    @Override
+    public String getDescription() {
+        return "All filters are executed once per request starting in Micronaut 3.0. Directly implement `HttpServerFilter` instead of extending `OncePerRequestHttpServerFilter` and replace any usages of `micronaut.once` attributes with a custom attribute name.";
+    }
+
+    @Override
+    protected @Nullable TreeVisitor<?, ExecutionContext> getSingleSourceApplicableTest() {
+        return new UsesType<>(ONCE_PER_REQUEST_FILTER_FQN);
+    }
+
+    @Override
+    protected OncePerRequestHttpServerFilterToHttpServerFilterVisitor getVisitor() {
+        return new OncePerRequestHttpServerFilterToHttpServerFilterVisitor();
+    }
+
+    private static class OncePerRequestHttpServerFilterToHttpServerFilterVisitor extends JavaIsoVisitor<ExecutionContext> {
+        private final MethodMatcher GET_KEY_METHOD = new MethodMatcher(ONCE_PER_REQUEST_FILTER_FQN + " getKey(Class<? extends OncePerRequestHttpServerFilter>)");
+
+        @Override
+        public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext executionContext) {
+            J.ClassDeclaration cd = super.visitClassDeclaration(classDecl, executionContext);
+            if (cd.getExtends() != null && cd.getExtends().getType() != null
+                    && TypeUtils.isOfClassType(cd.getExtends().getType(), ONCE_PER_REQUEST_FILTER_FQN)) {
+                cd = cd.withExtends(null);
+                J.Identifier newImplementsIdentifier = J.Identifier.build(UUID.randomUUID(), Space.format(" "), Markers.EMPTY,
+                        "HttpServerFilter", JavaType.buildType("io.micronaut.http.filter.HttpServerFilter"));
+                cd = cd.withImplements(ListUtils.concat(cd.getImplements(), newImplementsIdentifier));
+                if (cd.getType() != null) {
+                    doAfterVisit(new ChangeMethodName(
+                            cd.getType().getFullyQualifiedName() + " doFilterOnce(io.micronaut.http.HttpRequest, io.micronaut.http.filter.ServerFilterChain)",
+                            "doFilter"));
+                }
+                maybeAddImport("io.micronaut.http.filter.HttpServerFilter");
+                maybeRemoveImport(ONCE_PER_REQUEST_FILTER_FQN);
+            }
+            return cd;
+        }
+
+        @Override
+        public J.MethodInvocation visitMethodInvocation(J.MethodInvocation method, ExecutionContext executionContext) {
+            J.MethodInvocation mi = super.visitMethodInvocation(method, executionContext);
+            String todoCommentText = "TODO: Replace with custom attribute name";
+            if (GET_KEY_METHOD.matches(mi) && mi.getComments().stream().noneMatch(c -> c.getText().equals(todoCommentText))) {
+                mi = mi.withComments(ListUtils.concat(mi.getComments(), new Comment(Comment.Style.BLOCK, todoCommentText, " ", Markers.EMPTY)));
+            }
+            return mi;
+        }
+    }
+}
---
+++ src/test/kotlin/org/openrewrite/java/micronaut/OncePerRequestHttpServerFilterToHttpServerFilterTest.kt
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.micronaut
+
+import org.junit.jupiter.api.Test
+import org.openrewrite.Recipe
+import org.openrewrite.java.JavaParser
+import org.openrewrite.java.JavaRecipeTest
+
+class OncePerRequestHttpServerFilterToHttpServerFilterTest : JavaRecipeTest {
+    override val recipe: Recipe
+        get() = OncePerRequestHttpServerFilterToHttpServerFilter()
+    override val parser: JavaParser
+        get() = JavaParser.fromJavaVersion()
+            .classpath("micronaut-http", "micronaut-core")
+            .dependsOn("""
+                package a.b;
+                public interface C {
+                    String getCName();
+                }
+            """)
+            .build()
+
+    @Test
+    fun simpleConversion() = assertChanged(
+        before = """
+            package a.b;
+            
+            import io.micronaut.http.filter.OncePerRequestHttpServerFilter;
+            import io.micronaut.core.order.Ordered;
+            import io.micronaut.http.HttpRequest;
+            import io.micronaut.http.MutableHttpResponse;
+            import io.micronaut.http.filter.ServerFilterChain;
+            import org.reactivestreams.Publisher;
+            
+            public class MyServerFilter extends OncePerRequestHttpServerFilter implements C {
+                @Override
+                public int getOrder() {
+                    return Ordered.LOWEST_PRECEDENCE;
+                }
+                
+                @Override
+                public Publisher<MutableHttpResponse<?>> doFilterOnce(HttpRequest<?> request, ServerFilterChain chain) {
+                    getKey(MyServerFilter.class);
+                }
+                
+                @Override
+                public String getCName() {
+                    return "cname";
+                }
+            }
+        """,
+        after = """
+            package a.b;
+            
+            import io.micronaut.core.order.Ordered;
+            import io.micronaut.http.HttpRequest;
+            import io.micronaut.http.MutableHttpResponse;
+            import io.micronaut.http.filter.HttpServerFilter;
+            import io.micronaut.http.filter.ServerFilterChain;
+            import org.reactivestreams.Publisher;
+            
+            public class MyServerFilter implements C, HttpServerFilter {
+                @Override
+                public int getOrder() {
+                    return Ordered.LOWEST_PRECEDENCE;
+                }
+                
+                @Override
+                public Publisher<MutableHttpResponse<?>> doFilter(HttpRequest<?> request, ServerFilterChain chain) {
+                    /*TODO: Replace with custom attribute name*/ getKey(MyServerFilter.class);
+                }
+                
+                @Override
+                public String getCName() {
+                    return "cname";
+                }
+            }
+        """
+    )
+}
