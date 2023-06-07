dd26d988017ace08db451aa94c6f62857d2ad5e0
Tim te Beek <tim@moderne.io>
2023-06-07 20:08:00 +0200

    Inline fromRuntimeClasspath and limit package scan
    Also only scan once instead of twice

--- src/test/java/org/openrewrite/java/micronaut/UpdateMicronautValidationTest.java
+++ src/test/java/org/openrewrite/java/micronaut/UpdateMicronautValidationTest.java
@@ -18,6 +18,7 @@ package org.openrewrite.java.micronaut;
 import org.intellij.lang.annotations.Language;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.config.Environment;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
@@ -26,215 +27,221 @@ import static org.openrewrite.gradle.Assertions.buildGradle;
 import static org.openrewrite.java.Assertions.*;
 import static org.openrewrite.maven.Assertions.pomXml;
 
-public class UpdateMicronautValidationTest implements RewriteTest {
+class UpdateMicronautValidationTest implements RewriteTest {
 
     @Override
     public void defaults(RecipeSpec spec) {
-        spec.parser(JavaParser.fromJavaVersion().classpathFromResources(new InMemoryExecutionContext(), "validation-api-2.*", "jakarta.validation-api-3.*", "jakarta.inject-api-2.*"));
-        //spec.recipeFromResource("/META-INF/rewrite/micronaut3-to-4.yml", "org.openrewrite.java.micronaut.UpdateMicronautPlatformBom", "org.openrewrite.java.micronaut.UpdateMicronautValidation");
-        spec.recipes(RewriteTest.fromRuntimeClasspath("org.openrewrite.java.micronaut.UpdateMicronautPlatformBom"), RewriteTest.fromRuntimeClasspath("org.openrewrite.java.micronaut.UpdateMicronautValidation"));
+        spec
+          .parser(JavaParser.fromJavaVersion().classpathFromResources(new InMemoryExecutionContext(),
+            "validation-api-2.*", "jakarta.validation-api-3.*", "jakarta.inject-api-2.*"))
+          .recipes(Environment.builder()
+            .scanRuntimeClasspath("org.openrewrite.java.micronaut")
+            .build()
+            .activateRecipes(
+              "org.openrewrite.java.micronaut.UpdateMicronautPlatformBom",
+              "org.openrewrite.java.micronaut.UpdateMicronautValidation"));
     }
 
     @Language("java")
     private final String annotatedJavaxClass = """
-            import jakarta.inject.Singleton;
-            import javax.validation.constraints.NotBlank;
-            
-            @Singleton
-            public class PersonService {
-                public void sayHello(@NotBlank String name) {
-                    System.out.println("Hello " + name);
-                }
-            }
-        """;
+      import jakarta.inject.Singleton;
+      import javax.validation.constraints.NotBlank;
+                
+      @Singleton
+      public class PersonService {
+          public void sayHello(@NotBlank String name) {
+              System.out.println("Hello " + name);
+          }
+      }
+      """;
 
     @Language("java")
     private final String annotatedJakartaClass = """
-            import jakarta.inject.Singleton;
-            import jakarta.validation.constraints.NotBlank;
-            
-            @Singleton
-            public class PersonService {
-                public void sayHello(@NotBlank String name) {
-                    System.out.println("Hello " + name);
-                }
-            }
-        """;
+      import jakarta.inject.Singleton;
+      import jakarta.validation.constraints.NotBlank;
+                
+      @Singleton
+      public class PersonService {
+          public void sayHello(@NotBlank String name) {
+              System.out.println("Hello " + name);
+          }
+      }
+      """;
 
     @Language("groovy")
     private final String buildGradleInitial = """
-            dependencies {
-                annotationProcessor("io.micronaut:micronaut-http-validation")
-                implementation("io.micronaut:micronaut-http-client")
-                implementation("io.micronaut:micronaut-jackson-databind")
-                implementation("io.micronaut:micronaut-validation")
-                runtimeOnly("ch.qos.logback:logback-classic")
-            }
-        """;
+      dependencies {
+          annotationProcessor("io.micronaut:micronaut-http-validation")
+          implementation("io.micronaut:micronaut-http-client")
+          implementation("io.micronaut:micronaut-jackson-databind")
+          implementation("io.micronaut:micronaut-validation")
+          runtimeOnly("ch.qos.logback:logback-classic")
+      }
+      """;
 
     @Language("groovy")
     private final String buildGradleExpected = """
-            dependencies {
-                annotationProcessor("io.micronaut.validation:micronaut-validation-processor")
-                implementation("io.micronaut:micronaut-http-client")
-                implementation("io.micronaut:micronaut-jackson-databind")
-                implementation("io.micronaut.validation:micronaut-validation")
-                runtimeOnly("ch.qos.logback:logback-classic")
-            }
-        """;
+          dependencies {
+              annotationProcessor("io.micronaut.validation:micronaut-validation-processor")
+              implementation("io.micronaut:micronaut-http-client")
+              implementation("io.micronaut:micronaut-jackson-databind")
+              implementation("io.micronaut.validation:micronaut-validation")
+              runtimeOnly("ch.qos.logback:logback-classic")
+          }
+      """;
 
     @Language("xml")
     private final String pomInitial = """
-            <project>
-                <groupId>com.mycompany.app</groupId>
-                <artifactId>my-app</artifactId>
-                <version>1</version>
-                <parent>
-                    <groupId>io.micronaut</groupId>
-                    <artifactId>micronaut-parent</artifactId>
-                    <version>3.9.1</version>
-                </parent>
-                <dependencies>
-                    <dependency>
-                        <groupId>io.micronaut</groupId>
-                        <artifactId>micronaut-http-client</artifactId>
-                        <scope>compile</scope>
-                    </dependency>
-                    <dependency>
-                        <groupId>io.micronaut</groupId>
-                        <artifactId>micronaut-http-server-netty</artifactId>
-                        <scope>compile</scope>
-                    </dependency>
-                    <dependency>
-                        <groupId>io.micronaut</groupId>
-                        <artifactId>micronaut-jackson-databind</artifactId>
-                        <scope>compile</scope>
-                    </dependency>
-                    <dependency>
-                        <groupId>io.micronaut</groupId>
-                        <artifactId>micronaut-validation</artifactId>
-                        <scope>compile</scope>
-                    </dependency>
-                    <dependency>
-                        <groupId>ch.qos.logback</groupId>
-                        <artifactId>logback-classic</artifactId>
-                        <scope>runtime</scope>
-                    </dependency>
-                </dependencies>
-                <build>
-                    <plugins>
-                        <plugin>
-                            <groupId>io.micronaut.build</groupId>
-                            <artifactId>micronaut-maven-plugin</artifactId>
-                        </plugin>
-                        <plugin>
-                            <groupId>org.apache.maven.plugins</groupId>
-                            <artifactId>maven-compiler-plugin</artifactId>
-                            <configuration>
-                                <annotationProcessorPaths combine.children="append">
-                                    <path>
-                                        <groupId>io.micronaut</groupId>
-                                        <artifactId>micronaut-http-validation</artifactId>
-                                        <version>${micronaut.version}</version>
-                                    </path>
-                                </annotationProcessorPaths>
-                                <compilerArgs>
-                                    <arg>-Amicronaut.processing.group=com.example</arg>
-                                    <arg>-Amicronaut.processing.module=demo</arg>
-                                </compilerArgs>
-                            </configuration>
-                        </plugin>
-                    </plugins>
-                </build>
-            </project>
-        """;
+      <project>
+          <groupId>com.mycompany.app</groupId>
+          <artifactId>my-app</artifactId>
+          <version>1</version>
+          <parent>
+              <groupId>io.micronaut</groupId>
+              <artifactId>micronaut-parent</artifactId>
+              <version>3.9.1</version>
+          </parent>
+          <dependencies>
+              <dependency>
+                  <groupId>io.micronaut</groupId>
+                  <artifactId>micronaut-http-client</artifactId>
+                  <scope>compile</scope>
+              </dependency>
+              <dependency>
+                  <groupId>io.micronaut</groupId>
+                  <artifactId>micronaut-http-server-netty</artifactId>
+                  <scope>compile</scope>
+              </dependency>
+              <dependency>
+                  <groupId>io.micronaut</groupId>
+                  <artifactId>micronaut-jackson-databind</artifactId>
+                  <scope>compile</scope>
+              </dependency>
+              <dependency>
+                  <groupId>io.micronaut</groupId>
+                  <artifactId>micronaut-validation</artifactId>
+                  <scope>compile</scope>
+              </dependency>
+              <dependency>
+                  <groupId>ch.qos.logback</groupId>
+                  <artifactId>logback-classic</artifactId>
+                  <scope>runtime</scope>
+              </dependency>
+          </dependencies>
+          <build>
+              <plugins>
+                  <plugin>
+                      <groupId>io.micronaut.build</groupId>
+                      <artifactId>micronaut-maven-plugin</artifactId>
+                  </plugin>
+                  <plugin>
+                      <groupId>org.apache.maven.plugins</groupId>
+                      <artifactId>maven-compiler-plugin</artifactId>
+                      <configuration>
+                          <annotationProcessorPaths combine.children="append">
+                              <path>
+                                  <groupId>io.micronaut</groupId>
+                                  <artifactId>micronaut-http-validation</artifactId>
+                                  <version>${micronaut.version}</version>
+                              </path>
+                          </annotationProcessorPaths>
+                          <compilerArgs>
+                              <arg>-Amicronaut.processing.group=com.example</arg>
+                              <arg>-Amicronaut.processing.module=demo</arg>
+                          </compilerArgs>
+                      </configuration>
+                  </plugin>
+              </plugins>
+          </build>
+      </project>
+      """;
 
     @Language("xml")
     private final String pomExpected = """
-            <project>
-                <groupId>com.mycompany.app</groupId>
-                <artifactId>my-app</artifactId>
-                <version>1</version>
-                <parent>
-                    <groupId>io.micronaut.platform</groupId>
-                    <artifactId>micronaut-parent</artifactId>
-                    <version>4.0.0-M2</version>
-                </parent>
-                <dependencies>
-                    <dependency>
-                        <groupId>io.micronaut</groupId>
-                        <artifactId>micronaut-http-client</artifactId>
-                        <scope>compile</scope>
-                    </dependency>
-                    <dependency>
-                        <groupId>io.micronaut</groupId>
-                        <artifactId>micronaut-http-server-netty</artifactId>
-                        <scope>compile</scope>
-                    </dependency>
-                    <dependency>
-                        <groupId>io.micronaut</groupId>
-                        <artifactId>micronaut-jackson-databind</artifactId>
-                        <scope>compile</scope>
-                    </dependency>
-                    <dependency>
-                        <groupId>io.micronaut.validation</groupId>
-                        <artifactId>micronaut-validation</artifactId>
-                        <scope>compile</scope>
-                    </dependency>
-                    <dependency>
-                        <groupId>jakarta.validation</groupId>
-                        <artifactId>jakarta.validation-api</artifactId>
-                    </dependency>
-                    <dependency>
-                        <groupId>ch.qos.logback</groupId>
-                        <artifactId>logback-classic</artifactId>
-                        <scope>runtime</scope>
-                    </dependency>
-                </dependencies>
-                <build>
-                    <plugins>
-                        <plugin>
-                            <groupId>io.micronaut.build</groupId>
-                            <artifactId>micronaut-maven-plugin</artifactId>
-                        </plugin>
-                        <plugin>
-                            <groupId>org.apache.maven.plugins</groupId>
-                            <artifactId>maven-compiler-plugin</artifactId>
-                            <configuration>
-                                <annotationProcessorPaths combine.children="append">
-                                    <path>
-                                        <groupId>io.micronaut.validation</groupId>
-                                        <artifactId>micronaut-validation-processor</artifactId>
-                                        <version>${micronaut.validation.version}</version>
-                                        <exclusions>
-                                            <exclusion>
-                                                <groupId>io.micronaut</groupId>
-                                                <artifactId>micronaut-inject</artifactId>
-                                            </exclusion>
-                                        </exclusions>
-                                    </path>
-                                </annotationProcessorPaths>
-                                <compilerArgs>
-                                    <arg>-Amicronaut.processing.group=com.example</arg>
-                                    <arg>-Amicronaut.processing.module=demo</arg>
-                                </compilerArgs>
-                            </configuration>
-                        </plugin>
-                    </plugins>
-                </build>
-            </project>
-        """;
+      <project>
+          <groupId>com.mycompany.app</groupId>
+          <artifactId>my-app</artifactId>
+          <version>1</version>
+          <parent>
+              <groupId>io.micronaut.platform</groupId>
+              <artifactId>micronaut-parent</artifactId>
+              <version>4.0.0-M2</version>
+          </parent>
+          <dependencies>
+              <dependency>
+                  <groupId>io.micronaut</groupId>
+                  <artifactId>micronaut-http-client</artifactId>
+                  <scope>compile</scope>
+              </dependency>
+              <dependency>
+                  <groupId>io.micronaut</groupId>
+                  <artifactId>micronaut-http-server-netty</artifactId>
+                  <scope>compile</scope>
+              </dependency>
+              <dependency>
+                  <groupId>io.micronaut</groupId>
+                  <artifactId>micronaut-jackson-databind</artifactId>
+                  <scope>compile</scope>
+              </dependency>
+              <dependency>
+                  <groupId>io.micronaut.validation</groupId>
+                  <artifactId>micronaut-validation</artifactId>
+                  <scope>compile</scope>
+              </dependency>
+              <dependency>
+                  <groupId>jakarta.validation</groupId>
+                  <artifactId>jakarta.validation-api</artifactId>
+              </dependency>
+              <dependency>
+                  <groupId>ch.qos.logback</groupId>
+                  <artifactId>logback-classic</artifactId>
+                  <scope>runtime</scope>
+              </dependency>
+          </dependencies>
+          <build>
+              <plugins>
+                  <plugin>
+                      <groupId>io.micronaut.build</groupId>
+                      <artifactId>micronaut-maven-plugin</artifactId>
+                  </plugin>
+                  <plugin>
+                      <groupId>org.apache.maven.plugins</groupId>
+                      <artifactId>maven-compiler-plugin</artifactId>
+                      <configuration>
+                          <annotationProcessorPaths combine.children="append">
+                              <path>
+                                  <groupId>io.micronaut.validation</groupId>
+                                  <artifactId>micronaut-validation-processor</artifactId>
+                                  <version>${micronaut.validation.version}</version>
+                                  <exclusions>
+                                      <exclusion>
+                                          <groupId>io.micronaut</groupId>
+                                          <artifactId>micronaut-inject</artifactId>
+                                      </exclusion>
+                                  </exclusions>
+                              </path>
+                          </annotationProcessorPaths>
+                          <compilerArgs>
+                              <arg>-Amicronaut.processing.group=com.example</arg>
+                              <arg>-Amicronaut.processing.module=demo</arg>
+                          </compilerArgs>
+                      </configuration>
+                  </plugin>
+              </plugins>
+          </build>
+      </project>
+      """;
 
 
     @Test
     void updateJavaCodeAndModifyGradleDependencies() {
         rewriteRun(mavenProject("project", srcMainJava(java(annotatedJavaxClass, annotatedJakartaClass)),
-                buildGradle(buildGradleInitial, buildGradleExpected)));
+          buildGradle(buildGradleInitial, buildGradleExpected)));
     }
 
     @Test
     void updateJavaCodeAndModifyMavenDependencies() {
         rewriteRun(mavenProject("project", srcMainJava(java(annotatedJavaxClass, annotatedJakartaClass)),
-                pomXml(pomInitial, pomExpected)));
+          pomXml(pomInitial, pomExpected)));
     }
 }
