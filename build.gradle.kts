53bb58c41bd745f38cde2ab1e23747642112e38a
Sam Snyder <sam@moderne.io>
2023-06-13 19:06:09 -0700

    Use latest rewrite-migrate-java and rewirte-java-dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,8 @@ dependencies {
     implementation("org.openrewrite:rewrite-gradle")
     implementation("org.openrewrite:rewrite-properties")
     implementation("org.openrewrite:rewrite-yaml")
-    implementation("org.openrewrite.recipe:rewrite-migrate-java:2.+")
-    implementation("org.openrewrite.recipe:rewrite-java-dependencies:1.+")
+    implementation("org.openrewrite.recipe:rewrite-migrate-java:${rewriteVersion}")
+    implementation("org.openrewrite.recipe:rewrite-java-dependencies:${rewriteVersion}")
 
     runtimeOnly("org.openrewrite:rewrite-java-8")
     runtimeOnly("org.openrewrite:rewrite-java-11")
