3b045dce0940900a1cf2e104879bd094b47b00cc
Tim te Beek <timtebeek@gmail.com>
2024-02-05 09:09:56 +0000

    refactor: Remove unused imports
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.java.RemoveUnusedImports?organizationId=T3BlblJld3JpdGU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/test/java/org/openrewrite/java/micronaut/AddHttpRequestTypeParameterTest.java
+++ src/test/java/org/openrewrite/java/micronaut/AddHttpRequestTypeParameterTest.java
@@ -21,7 +21,7 @@ import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
-import static org.openrewrite.java.Assertions.*;
+import static org.openrewrite.java.Assertions.java;
 
 class AddHttpRequestTypeParameterTest implements RewriteTest {
 
