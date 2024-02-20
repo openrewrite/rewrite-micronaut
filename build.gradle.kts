a4078ee69606583421dd198def4f25c3e1489846
Jonathan Schneider <jkschneider@gmail.com>
2024-02-20 06:46:36 -0500

    Add org.openrewrite.gradle.tooling:model

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
 
     testImplementation("org.openrewrite:rewrite-test")
     testImplementation("org.openrewrite:rewrite-java-tck")
+    testImplementation("org.openrewrite.gradle.tooling:model:$rewriteVersion")
     testImplementation("org.assertj:assertj-core:latest.release")
     testRuntimeOnly(gradleApi())
 }
