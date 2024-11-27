52691be0faef72bf9eda71e0241b4288689809f4
Tim te Beek <tim@moderne.io>
2024-11-27 11:55:07 +0100

    Use non-Atomic boolean in Accumulator

--- src/main/java/org/openrewrite/java/micronaut/AddSnakeYamlDependencyIfNeeded.java
+++ src/main/java/org/openrewrite/java/micronaut/AddSnakeYamlDependencyIfNeeded.java
@@ -15,12 +15,11 @@
  */
 package org.openrewrite.java.micronaut;
 
-import lombok.Value;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.jspecify.annotations.Nullable;
 import org.openrewrite.*;
 import org.openrewrite.java.dependencies.AddDependency;
-
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class AddSnakeYamlDependencyIfNeeded extends ScanningRecipe<AddSnakeYamlDependencyIfNeeded.Accumulator> {
 
@@ -36,7 +35,7 @@ public class AddSnakeYamlDependencyIfNeeded extends ScanningRecipe<AddSnakeYamlD
 
     @Override
     public AddSnakeYamlDependencyIfNeeded.Accumulator getInitialValue(ExecutionContext ctx) {
-        return new Accumulator(addDependencyRecipe().getInitialValue(ctx));
+        return new Accumulator(false, addDependencyRecipe().getInitialValue(ctx));
     }
 
     @Override
@@ -46,8 +45,8 @@ public class AddSnakeYamlDependencyIfNeeded extends ScanningRecipe<AddSnakeYamlD
             @Override
             public @Nullable Tree visit(@Nullable Tree tree, ExecutionContext ctx) {
                 if (tree instanceof SourceFile) {
-                    if (!acc.usesYamlConfig.get()) {
-                        acc.usesYamlConfig.set(tree != new FindYamlConfig().getVisitor().visit(tree, ctx));
+                    if (!acc.usesYamlConfig) {
+                        acc.usesYamlConfig = (tree != new FindYamlConfig().getVisitor().visit(tree, ctx));
                     }
                     TreeVisitor<?, ExecutionContext> addDependencyScanner = addDependencyRecipe.getScanner(acc.getAddDependencyAccumulator());
                     if (addDependencyScanner.isAcceptable((SourceFile) tree, ctx)) {
@@ -61,7 +60,7 @@ public class AddSnakeYamlDependencyIfNeeded extends ScanningRecipe<AddSnakeYamlD
 
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor(AddSnakeYamlDependencyIfNeeded.Accumulator acc) {
-        return Preconditions.check(acc.usesYamlConfig.get(), addDependencyRecipe().getVisitor(acc.getAddDependencyAccumulator()));
+        return Preconditions.check(acc.usesYamlConfig, addDependencyRecipe().getVisitor(acc.getAddDependencyAccumulator()));
     }
 
     private static AddDependency addDependencyRecipe() {
@@ -70,9 +69,10 @@ public class AddSnakeYamlDependencyIfNeeded extends ScanningRecipe<AddSnakeYamlD
                 null, null, null, "runtimeOnly", "runtime", null, null, null, null);
     }
 
-    @Value
+    @Data
+    @AllArgsConstructor
     public static class Accumulator {
-        AtomicBoolean usesYamlConfig = new AtomicBoolean(false);
+        boolean usesYamlConfig;
         AddDependency.Accumulator addDependencyAccumulator;
     }
 }
