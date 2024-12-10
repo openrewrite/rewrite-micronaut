0804dca8e26d7bd5cdb2a30891db0bc326f41995
Tim te Beek <tim@moderne.io>
2024-12-10 19:27:38 +0100

    Add missing tools API to a gradle dependency test

--- src/test/java/org/openrewrite/java/micronaut/UpdateMicronautSessionTest.java
+++ src/test/java/org/openrewrite/java/micronaut/UpdateMicronautSessionTest.java
@@ -20,6 +20,7 @@ import org.openrewrite.config.Environment;
 import org.openrewrite.test.RecipeSpec;
 
 import static org.openrewrite.gradle.Assertions.buildGradle;
+import static org.openrewrite.gradle.toolingapi.Assertions.withToolingApi;
 import static org.openrewrite.java.Assertions.mavenProject;
 import static org.openrewrite.maven.Assertions.pomXml;
 
@@ -36,10 +37,14 @@ class UpdateMicronautSessionTest extends Micronaut4RewriteTest {
     @Test
     void updateGradleDependencies() {
         rewriteRun(
+          spec -> spec.beforeRecipe(withToolingApi()),
           mavenProject("project",
             //language=groovy
             buildGradle(
               """
+                plugins {
+                    id "java-library"
+                }
                 dependencies {
                     implementation("io.micronaut:micronaut-session")
                 }
@@ -48,6 +53,9 @@ class UpdateMicronautSessionTest extends Micronaut4RewriteTest {
                 }
                 """,
               """
+                plugins {
+                    id "java-library"
+                }
                 dependencies {
                     implementation("io.micronaut.session:micronaut-session")
                 }
